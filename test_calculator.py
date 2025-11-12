bb07caaaae5006806930499cb6b5e35572f1d41c
Jason <ljslayer57@gmail.com>
2025-11-12 16:34:42 -0500

    gradescope chanegs2

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,11 @@
-import unittest
-from calculator import *
-
 # https://github.com/JasonShermanII/lab11-JS-MG
 # Partner 1: Matthew Grizio
 # Parner 2: Jason Sherman
+
+import unittest
+from calculator import *
+
+
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -13,9 +15,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(5,5),10)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(10,8),2)
-        self.assertEqual(sub(4,2),2)
-        self.assertEqual(sub(6,3),3)
+        self.assertEqual(subtract(10,8),2)
+        self.assertEqual(subtract(4,2),2)
+        self.assertEqual(subtract(6,3),3)
     # ##########################
 
     ######## Partner 1
@@ -25,9 +27,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(mul(5, 0), 0)
 
     def test_divide(self): # 3 assertions
-        self.assertEqual(div(6, 3), 2)
-        self.assertEqual(div(10, 1), 10)
-        self.assertEqual(div(20,4), 5)
+        self.assertEqual(div(3, 6), 2)
+        self.assertEqual(div(1, 10), 10)
+        self.assertEqual(div(4,20), 5)
     # ##########################
 
     ######## Partner 2
@@ -38,20 +40,20 @@ class TestCalculator(unittest.TestCase):
 
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(log(2,8),3)
-        self.assertAlmostEqual(log(10,10),10)
-        self.assertAlmostEqual(log(5,1),0.0)
+        self.assertAlmostEqual(logarithm(2,8),3)
+        self.assertAlmostEqual(logarithm(10,10),1)
+        self.assertAlmostEqual(logarithm(100,10),2)
 
     def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
         with self.assertRaises(ValueError):
-            log(0, 10)
+            logarithm(0, 10)
     # ##########################
     
     ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         with self.assertRaises(ValueError):
-            self.log(0, 10)
+            logarithm(0, 10)
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
@@ -68,7 +70,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(square_root(1), 1)
     #     # Test for invalid argument, example:
         with self.assertRaises(ValueError):
-            self.square_root(-4)
+            square_root(-4)
     #     #    square_root(NUM)
     #     # Test basic function
     #     fill in code
